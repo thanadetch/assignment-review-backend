c303950362d7b2df4375c6028a8ef2f5ca0831d8
Thanadetch Lertwuthikarn <tlertwuthikarn@palo-it.com>
2025-04-19 14:38:56 +0700

    feat: NestJS application with basic structure and configuration

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from '../prisma/prisma.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), UsersModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
