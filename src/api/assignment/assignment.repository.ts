dafa58bfa41ff4de1278c7c7ab89bd435d364705
Thanadetch Lertwuthikarn <thanadetch.lert@gmail.com>
2025-04-26 04:49:10 +0700

    feat: include user information in assignment queries

--- src/api/assignment/assignment.repository.ts
+++ src/api/assignment/assignment.repository.ts
@@ -21,6 +21,7 @@ export class AssignmentRepository {
       where: query,
       include: {
         masterAssignment: true,
+        user: true,
         reviews: {
           include: {
             comments: {
